46014ee4f515ea15328a12c2cdd290b967b44627
Vitaliy <vitaliy.g989@gmail.com>
2019-09-03 23:28:01 +0300

    version 1.4

--- src/by/epam/classesAndObjects/task3/Main.java
+++ src/by/epam/classesAndObjects/task3/Main.java
@@ -9,17 +9,16 @@ package by.epam.classesAndObjects.task3;
 public class Main {
 
 	public static void main(String[] args) {
-		Student[] student = new Student[10];
+		Student[] student = new Student[9];
 		student[0] = new Student("Gubin", "V. O.", 10903517, 9);
 		student[1] = new Student("Mager", "O. S.", 10903516, 7);
-		student[2] = new Student("Kabak", "E. C.", 10204117, 8);
-		student[3] = new Student("Renat", "V. Z.", 10902311, 7);
-		student[4] = new Student("Kasperovich", "E. S.", 10902118, 6);
-		student[5] = new Student("Avdechik", "V. S.", 10904516, 7);
-		student[6] = new Student("Vinogradova", "M. K.", 10905612, 9);
-		student[7] = new Student("Rajko", "A. O.", 10702617, 6);
-		student[8] = new Student("Morozov", "P. I.", 10301116, 4);
-		student[9] = new Student("Erema", "Z. D.", 10405715, 10);
+		student[2] = new Student("Renat", "V. Z.", 10902311, 7);
+		student[3] = new Student("Kasperovich", "E. S.", 10902118, 6);
+		student[4] = new Student("Avdechik", "V. S.", 10904516, 7);
+		student[5] = new Student("Vinogradova", "M. K.", 10905612, 9);
+		student[6] = new Student("Rajko", "A. O.", 10702617, 6);
+		student[7] = new Student("Morozov", "P. I.", 10301116, 4);
+		student[8] = new Student("Erema", "Z. D.", 10405715, 10);
 		getBestProgress(student);
 	}
 
